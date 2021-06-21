677b2abf22cf0450d00ee709fa0529637c4e6162
AaronPacht <aharonpacht@gmail.com>
2021-06-21 17:49:44 -0400

    added method to check for negative

--- ruby_fundamentals2.rb
+++ ruby_fundamentals2.rb
@@ -3,3 +3,9 @@
 def double(my_number)
     return my_number*2
 end
+
+# Define a method called negative? that accepts a number as an argument and returns a boolean (true/false) indicating whether that number is negative or not.
+
+def negative?(num)
+    return num<0
+end
