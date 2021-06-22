0f81e968b19b9de96e8130dc3e1e645c7ab77863
AaronPacht <aharonpacht@gmail.com>
2021-06-22 19:42:50 -0400

    completed exercise_6

---
+++ exercise_6.rb
@@ -0,0 +1,23 @@
+def area(length,width)
+    return length*width
+end
+
+def speed(dist,time)
+    return dist/time
+end
+
+def circumference(radius)
+    return 2*3.14159*radius
+end
+
+def volume(radius)
+    return 4.0/3*3.14159*(radius**3)
+end
+
+def hypotenuse(one,two)
+    return Math.sqrt(one**2+two**2)
+end
+
+def energy(m)
+    return m*299792458**2
+end
