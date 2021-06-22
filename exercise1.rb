bbb4679b11c817968b97f9ad1c181950dee69b65
AaronPacht <aharonpacht@gmail.com>
2021-06-22 16:50:32 -0400

    completed exercise1

---
+++ exercise1.rb
@@ -0,0 +1,12 @@
+def multiply_3(one,two,three)
+    return one*two*three
+end
+
+def cube(num)
+    return num*num*num
+end
+
+def impress_friends(num)
+    cubed=num*num*num
+    return "I know numbers bigger than #{cubed}, do you?"
+end
